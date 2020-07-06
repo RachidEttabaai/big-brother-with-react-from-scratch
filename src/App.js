883c4a7a592e0cf4fb0fe72715e80a94bb1f8ae5
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2020-07-06 13:22:24 +0200

    update npm

--- src/App.js
+++ src/App.js
@@ -3,40 +3,56 @@ import getDatas from "./components/Datas";
 import Persons from "./components/Persons";
 import MappingPersons from "./components/MappingPersons";
 import Home from './components/Home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar,Nav} from "react-bootstrap";
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Nav } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function App(){
+export default function App() {
 
-    return(
-        <>
-            <Router>
+    return ( < >
+        <
+        Router >
 
-            <Navbar expand="lg" className="justify-content-center">
-                <Navbar.Brand><Link to="/">Chasing</Link></Navbar.Brand>
-                <Navbar>
-                    <Nav className="mr-auto">
-                    <Nav.Link as={Link} to="/personlists">Listing Persons</Nav.Link>
-                    <Nav.Link as={Link} to="/mappingpersons">Mapping Persons</Nav.Link>
-                    </Nav>
-                </Navbar>
-            </Navbar>
-                
-                <Switch>
-                    <Route exact path="/personlists">
-                        <Persons persons={getDatas()}/>
-                    </Route>
-                    <Route path="/mappingpersons">
-                        <MappingPersons persons={getDatas()}/>
-                    </Route>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            
-            </Router>
-        </>
+        <
+        Navbar expand = "lg"
+        className = "justify-content-center" >
+        <
+        Navbar.Brand > < Link to = "/" > Chasing < /Link></Navbar.Brand >
+        <
+        Navbar >
+        <
+        Nav className = "mr-auto" >
+        <
+        Nav.Link as = { Link }
+        to = "/personlists" > Listing Persons < /Nav.Link> <
+        Nav.Link as = { Link }
+        to = "/mappingpersons" > Mapping Persons < /Nav.Link> < /
+        Nav > <
+        /Navbar> < /
+        Navbar >
+
+        <
+        Switch >
+        <
+        Route exact path = "/personlists" >
+        <
+        Persons persons = { getDatas() }
+        /> < /
+        Route > <
+        Route path = "/mappingpersons" >
+        <
+        MappingPersons persons = { getDatas() }
+        /> < /
+        Route > <
+        Route exact path = "/" >
+        <
+        Home / >
+        <
+        /Route> < /
+        Switch >
+
+        <
+        /Router> < / >
     );
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const webpack_dashboard = require("webpack-dashboard/plugin");
 
-let entry = "./src/app.js";
+let entry = "./src/index.js";
 let bundlefile = "./bundle.js";
 let publicfolder = "./public"
 let mode = "development"
@@ -10,22 +10,30 @@ let mode = "development"
 let config = {
     mode: mode,
     entry: entry,
-    output:{
-        path: path.resolve(__dirname,publicfolder),
+    output: {
+        path: path.resolve(__dirname, publicfolder),
         filename: bundlefile
     },
-    plugins:[new webpack_dashboard()],
+    plugins: [new webpack_dashboard()],
     module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
+        rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
-          }
         ]
     }
+
 }
 
 module.exports = config
